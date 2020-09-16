a7dd8575a1dab5d4686bef545019854c69155322
fordShissanupong <ford19982541@gmail.com>
2020-09-16 14:35:10 +0700

    Dev.ford : new map function .

--- js/loop.js
+++ js/loop.js
@@ -1,7 +1,14 @@
-for (let i=1 ; i <= 20 ; i++){
-    console.log(i)
-}
+// for (let i=1 ; i <= 20 ; i++){
+//     console.log(i)
+// }
 let temp =['apple', 'banana', 'kiwi']
-temp.forEach(element =>{
-    console.log(element)
+
+// temp.forEach((element, idx) =>{
+//     console.log(idx, element)
+// })
+
+let newValue =temp.map((element, idx) => {
+    return `${idx}.${element}`
 })
+
+console.log(newValue)
